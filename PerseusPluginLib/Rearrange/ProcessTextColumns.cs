609d3667741412704404d6afd3afe62b07a7acf4
Nadin Neuhauser <neuhause@biochem.mpg.de>
2013-12-02 08:12:24 -0800

    Add parameter 'Keep original columns' to feature 'Process text column'.

--- PerseusPluginLib/Rearrange/ProcessTextColumns.cs
+++ PerseusPluginLib/Rearrange/ProcessTextColumns.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Windows.Media;
 using BaseLib.Param;
@@ -8,51 +7,60 @@ using PerseusApi.Generic;
 using PerseusApi.Matrix;
 
 namespace PerseusPluginLib.Rearrange{
-	public class ProcessTextColumns : IMatrixProcessing{
-		public bool HasButton { get { return false; } }
-		public ImageSource ButtonImage { get { return null; } }
-		public string HelpDescription { get { return "Values in string columns can be manipulated according to a regular expression."; } }
-		public DocumentType HelpDescriptionType { get { return DocumentType.PlainText; } }
-		public string HelpOutput { get { return ""; } }
-		public string[] HelpSupplTables { get { return new string[0]; } }
-		public DocumentType HelpOutputType { get { return DocumentType.PlainText; } }
-		public DocumentType[] HelpSupplTablesType { get { return new DocumentType[0]; } }
-		public int NumSupplTables { get { return 0; } }
-		public string Name { get { return "Process text column"; } }
-		public string Heading { get { return "Matrix rearrangements"; } }
-		public bool IsActive { get { return true; } }
-		public float DisplayOrder { get { return 22; } }
-		public string[] HelpDocuments { get { return new string[0]; } }
-		public DocumentType[] HelpDocumentTypes { get { return new DocumentType[0]; } }
-		public int NumDocuments { get { return 0; } }
+    public class ProcessTextColumns : IMatrixProcessing{
+        public bool HasButton { get { return false; } }
+        public ImageSource ButtonImage { get { return null; } }
+        public string HelpDescription { get { return "Values in string columns can be manipulated according to a regular expression."; } }
+        public DocumentType HelpDescriptionType { get { return DocumentType.PlainText; } }
+        public string HelpOutput { get { return ""; } }
+        public string[] HelpSupplTables { get { return new string[0]; } }
+        public DocumentType HelpOutputType { get { return DocumentType.PlainText; } }
+        public DocumentType[] HelpSupplTablesType { get { return new DocumentType[0]; } }
+        public int NumSupplTables { get { return 0; } }
+        public string Name { get { return "Process text column"; } }
+        public string Heading { get { return "Matrix rearrangements"; } }
+        public bool IsActive { get { return true; } }
+        public float DisplayOrder { get { return 22; } }
+        public string[] HelpDocuments { get { return new string[0]; } }
+        public DocumentType[] HelpDocumentTypes { get { return new DocumentType[0]; } }
+        public int NumDocuments { get { return 0; } }
 
-		public int GetMaxThreads(Parameters parameters) {
-			return 1;
-		}
+        public int GetMaxThreads(Parameters parameters){
+            return 1;
+        }
 
-		public void ProcessData(IMatrixData mdata, Parameters param, ref IMatrixData[] supplTables,
-			ref IDocumentData[] documents, ProcessInfo processInfo){
-			string regexStr = param.GetStringParam("Regular expression").Value;
-			Regex regex = new Regex(regexStr);
-			int[] inds = param.GetMultiChoiceParam("Columns").Value;
-			foreach (int ind in inds){
-				ProcessCol(mdata.StringColumns[ind], regex);
-			}
-		}
+        public void ProcessData(IMatrixData mdata, Parameters param, ref IMatrixData[] supplTables,
+                                ref IDocumentData[] documents, ProcessInfo processInfo){
+            string regexStr = param.GetStringParam("Regular expression").Value;
+            Regex regex = new Regex(regexStr);
+            int[] inds = param.GetMultiChoiceParam("Columns").Value;
+            bool keepColumns = param.GetBoolParam("Keep original columns").Value;
 
-		private static void ProcessCol(IList<string> col, Regex regex){
-			for (int i = 0; i < col.Count; i++){
-				string newVal = regex.Match(col[i]).Groups[1].ToString();
-				col[i] = newVal;
-			}
-		}
+            foreach (int col in inds){
+                ProcessCol(mdata, regex, col, keepColumns);
+            }
+        }
 
-		public Parameters GetParameters(IMatrixData mdata, ref string errorString) {
-			return
-				new Parameters(new Parameter[]{
-					new MultiChoiceParam("Columns"){Values = mdata.StringColumnNames},
-					new StringParam("Regular expression", "^([^;]+)")
-				});
-		}
-	}
+        private static void ProcessCol(IMatrixData mdata, Regex regex, int col, bool keepColumns){
+            string[] values = new string[mdata.RowCount];
+            for (int row = 0; row < mdata.RowCount; row++){
+                values[row] = regex.Match(mdata.StringColumns[col][row]).Groups[1].ToString();
+            }
+            if (keepColumns){
+                mdata.AddStringColumn(mdata.StringColumnNames[col], null, values);
+            }
+            else{
+                mdata.StringColumns[col] = values;
+            }
+        }
+
+        public Parameters GetParameters(IMatrixData mdata, ref string errorString){
+            return
+                new Parameters(new Parameter[]{
+                    new MultiChoiceParam("Columns"){Values = mdata.StringColumnNames},
+                    new StringParam("Regular expression", "^([^;]+)"),
+                    new BoolParam("Keep original columns", false)
+                });
+        }
+    }
 }
