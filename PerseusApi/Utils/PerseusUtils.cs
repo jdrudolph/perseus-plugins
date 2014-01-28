5b28bc6460fc5f32b503b21e1cc814f834c70a4a
Jurgen <jurgen.cox@gmail.com>
2014-01-28 21:40:38 +0100

    .

--- PerseusApi/Utils/PerseusUtils.cs
+++ PerseusApi/Utils/PerseusUtils.cs
@@ -3,11 +3,62 @@ using System.Collections.Generic;
 using System.IO;
 using BaseLib.Parse;
 using BaseLib.Util;
+using BaseLib.Wpf;
 using PerseusApi.Generic;
 using PerseusApi.Matrix;
 
 namespace PerseusApi.Utils{
 	public static class PerseusUtils{
+		public static readonly HashSet<string> categoricalColDefaultNames =
+			new HashSet<string>(new[]{
+				"pfam names", "gocc names", "gomf names", "gobp names", "kegg pathway names", "chromosome", "strand",
+				"interpro name", "prints name", "prosite name", "smart name", "sequence motifs", "reactome", "transcription factors"
+				, "microrna", "scop class", "scop fold", "scop superfamily", "scop family", "phospho motifs", "mim", "pdb", "intact"
+				, "corum", "motifs", "best motif", "reverse", "contaminant", "only identified by site", "type", "amino acid",
+				"raw file", "experiment", "charge", "modifications", "md modification", "dp aa", "dp decoy", "dp modification",
+				"fraction", "dp cluster index", "authors", "publication", "year", "publisher", "geography", "geography id",
+				"identified", "fragmentation", "mass analyzer", "labeling state"
+			});
+		public static readonly HashSet<string> textualColDefaultNames =
+			new HashSet<string>(new[]{
+				"protein ids", "majority protein ids", "protein names", "gene names", "uniprot", "ensembl", "ensg", "ensp", "enst",
+				"mgi", "kegg ortholog", "dip", "hprd interactors", "sequence window", "sequence", "orf name", "names", "proteins",
+				"positions within proteins", "leading proteins", "md sequence", "md proteins", "md gene names", "md protein names",
+				"dp base sequence", "dp probabilities", "dp proteins", "dp gene names", "dp protein names", "name", "dn sequence",
+				"title", "volume", "number", "pages", "modified sequence"
+			});
+		public static readonly HashSet<string> numericColDefaultNames =
+			new HashSet<string>(new[]{
+				"length", "position", "total position", "peptides (seq)", "razor peptides (seq)", "unique peptides (seq)",
+				"localization prob", "size", "p value", "benj. hoch. fdr", "score", "delta score", "combinatorics", "intensity",
+				"score for localization", "pep", "m/z", "mass", "resolution", "uncalibrated - calibrated m/z [ppm]",
+				"mass error [ppm]", "uncalibrated mass error [ppm]", "uncalibrated - calibrated m/z [da]", "mass error [da]",
+				"uncalibrated mass error [da]", "max intensity m/z 0", "retention length", "retention time",
+				"calibrated retention time", "calibrated retention time start", "calibrated retention time finish",
+				"retention time calibration", "match time difference", "match q-value", "match score", "number of data points",
+				"number of scans", "number of isotopic peaks", "pif", "fraction of total spectrum", "base peak fraction",
+				"ms/ms count", "ms/ms m/z", "md base scan number", "md mass error", "md time difference", "dp mass difference",
+				"dp time difference", "dp score", "dp pep", "dp positional probability", "dp base scan number", "dp mod scan number"
+				, "dp cluster mass", "dp cluster mass sd", "dp cluster size total", "dp cluster size forward",
+				"dp cluster size reverse", "dp peptide length difference", "dn score", "dn normalized score", "dn nterm mass",
+				"dn cterm mass", "dn score diff", "views", "estimated minutes watched", "average view duration",
+				"average percentage viewed", "subscriber views", "subscriber minutes watched", "clicks", "clickable impressions",
+				"click through rate", "closes", "closable impressions", "close rate", "impressions", "likes", "likes added",
+				"likes removed", "dislikes", "dislikes added", "dislikes removed", "shares", "comments", "favorites",
+				"favorites added", "favorites removed", "subscribers", "subscribers gained", "subscribers lost",
+				"average view duration (minutes)", "scan number", "ion injection time", "total ion current", "base peak intensity",
+				"elapsed time", "precursor full scan number", "precursor intensity", "precursor apex fraction",
+				"precursor apex offset", "precursor apex offset time", "scan event number", "scan index", "ms scan index",
+				"ms scan number", "agc fill", "parent intensity fraction", "intens comp factor", "ctcd comp", "rawovftt",
+				"cycle time", "dead time", "basepeak intensity", "mass calibration", "peak length", "isotope pattern length",
+				"multiplet length", "peaks / s", "single peaks / s", "isotope patterns / s", "single isotope patterns / s",
+				"multiplets / s", "identified multiplets / s", "multiplet identification rate [%]", "ms/ms / s",
+				"identified ms/ms / s", "ms/ms identification rate [%]", "mass fractional part", "mass deficit",
+				"mass precision [ppm]", "max intensity m/z 1", "retention length (fwhm)", "min scan number", "max scan number",
+				"lys count", "arg count", "intensity", "intensity h", "intensity m", "intensity l", "r count", "k count"
+			});
+		public static readonly HashSet<string> multiNumericColDefaultNames =
+			new HashSet<string>(new[]{"mass deviations [da]", "mass deviations [ppm]", "number of phospho (sty)"});
 		public static readonly HashSet<string> commentPrefix = new HashSet<string>(new[]{"#", "!"});
 		public static readonly HashSet<string> commentPrefixExceptions = new HashSet<string>(new[]{"#N/A", "#n/a"});
 
@@ -53,10 +104,10 @@ namespace PerseusApi.Utils{
 				processInfo.Progress, processInfo.Status, separator, reader, nrows);
 		}
 
-		private static void LoadMatrixData(IList<string> colNames, IList<string> colDescriptions, IList<int> expressionColIndices,
-			IList<int> catColIndices, IList<int> numColIndices, IList<int> textColIndices, IList<int> multiNumColIndices,
-			string origin, IMatrixData matrixData, IDictionary<string, string[]> annotationRows, Action<int> progress,
-			Action<string> status, char separator, TextReader reader, int nrows){
+		private static void LoadMatrixData(IList<string> colNames, IList<string> colDescriptions,
+			IList<int> expressionColIndices, IList<int> catColIndices, IList<int> numColIndices, IList<int> textColIndices,
+			IList<int> multiNumColIndices, string origin, IMatrixData matrixData, IDictionary<string, string[]> annotationRows,
+			Action<int> progress, Action<string> status, char separator, TextReader reader, int nrows){
 			Dictionary<string, string[]> catAnnotatRows;
 			Dictionary<string, string[]> numAnnotatRows;
 			status("Reading data");
@@ -291,6 +342,80 @@ namespace PerseusApi.Utils{
 				w[i] = s;
 			}
 			return w;
+		}
+
+		public static void SelectExact(ICollection<string> colNames, IList<string> colTypes, IList<bool> colVisible,
+			MultiListSelectorControl mls){
+			for (int i = 0; i < colNames.Count; i++){
+				if (colVisible == null || colVisible[i]){
+					switch (colTypes[i]){
+						case "E":
+							mls.SetSelected(0, i, true);
+							break;
+						case "N":
+							mls.SetSelected(1, i, true);
+							break;
+						case "C":
+							mls.SetSelected(2, i, true);
+							break;
+						case "T":
+							mls.SetSelected(3, i, true);
+							break;
+						case "M":
+							mls.SetSelected(4, i, true);
+							break;
+						default:
+							throw new Exception("Unknown type: " + colTypes[i]);
+					}
+				}
+			}
+		}
+
+		public static void SelectHeuristic(IList<string> colNames, MultiListSelectorControl mls){
+			char guessedType = GuessSilacType(colNames);
+			for (int i = 0; i < colNames.Count; i++){
+				if (categoricalColDefaultNames.Contains(colNames[i].ToLower())){
+					mls.SetSelected(2, i, true);
+					continue;
+				}
+				if (textualColDefaultNames.Contains(colNames[i].ToLower())){
+					mls.SetSelected(3, i, true);
+					continue;
+				}
+				if (numericColDefaultNames.Contains(colNames[i].ToLower())){
+					mls.SetSelected(1, i, true);
+					continue;
+				}
+				if (multiNumericColDefaultNames.Contains(colNames[i].ToLower())){
+					mls.SetSelected(4, i, true);
+					continue;
+				}
+				switch (guessedType){
+					case 's':
+						if (colNames[i].StartsWith("Norm. Intensity")){
+							mls.SetSelected(0, i, true);
+						}
+						break;
+					case 'd':
+						if (colNames[i].StartsWith("Ratio H/L Normalized ")){
+							mls.SetSelected(0, i, true);
+						}
+						break;
+				}
+			}
+		}
+
+		public static char GuessSilacType(IEnumerable<string> colnames){
+			bool isSilac = false;
+			foreach (string s in colnames){
+				if (s.StartsWith("Ratio M/L")){
+					return 't';
+				}
+				if (s.StartsWith("Ratio H/L")){
+					isSilac = true;
+				}
+			}
+			return isSilac ? 'd' : 's';
 		}
 	}
 }
--- PerseusPluginLib/Load/PerseusLoadParameterControl.xaml.cs
+++ PerseusPluginLib/Load/PerseusLoadParameterControl.xaml.cs
@@ -12,80 +12,29 @@ namespace PerseusPluginLib.Load{
 	/// Interaction logic for PerseusLoadParameterControl.xaml
 	/// </summary>
 	public partial class PerseusLoadParameterControl : UserControl{
-		private static readonly HashSet<string> categoricalCols =
-			new HashSet<string>(new[]{
-				"pfam names", "gocc names", "gomf names", "gobp names", "kegg pathway names", "chromosome", "strand",
-				"interpro name", "prints name", "prosite name", "smart name", "sequence motifs", "reactome", "transcription factors"
-				, "microrna", "scop class", "scop fold", "scop superfamily", "scop family", "phospho motifs", "mim", "pdb", "intact"
-				, "corum", "motifs", "best motif", "reverse", "contaminant", "only identified by site", "type", "amino acid",
-				"raw file", "experiment", "charge", "modifications", "md modification", "dp aa", "dp decoy", "dp modification",
-				"fraction", "dp cluster index", "authors", "publication", "year", "publisher", "geography", "geography id",
-				"identified", "fragmentation", "mass analyzer", "labeling state"
-			});
-		private static readonly HashSet<string> textualCols =
-			new HashSet<string>(new[]{
-				"protein ids", "majority protein ids", "protein names", "gene names", "uniprot", "ensembl", "ensg", "ensp", "enst",
-				"mgi", "kegg ortholog", "dip", "hprd interactors", "sequence window", "sequence", "orf name", "names", "proteins",
-				"positions within proteins", "leading proteins", "md sequence", "md proteins", "md gene names", "md protein names",
-				"dp base sequence", "dp probabilities", "dp proteins", "dp gene names", "dp protein names", "name", "dn sequence",
-				"title", "volume", "number", "pages", "modified sequence"
-			});
-		private static readonly HashSet<string> numericCols =
-			new HashSet<string>(new[]{
-				"length", "position", "total position", "peptides (seq)", "razor peptides (seq)", "unique peptides (seq)",
-				"localization prob", "size", "p value", "benj. hoch. fdr", "score", "delta score", "combinatorics", "intensity",
-				"score for localization", "pep", "m/z", "mass", "resolution", "uncalibrated - calibrated m/z [ppm]",
-				"mass error [ppm]", "uncalibrated mass error [ppm]", "uncalibrated - calibrated m/z [da]", "mass error [da]",
-				"uncalibrated mass error [da]", "max intensity m/z 0", "retention length", "retention time",
-				"calibrated retention time", "calibrated retention time start", "calibrated retention time finish",
-				"retention time calibration", "match time difference", "match q-value", "match score", "number of data points",
-				"number of scans", "number of isotopic peaks", "pif", "fraction of total spectrum", "base peak fraction",
-				"ms/ms count", "ms/ms m/z", "md base scan number", "md mass error", "md time difference", "dp mass difference",
-				"dp time difference", "dp score", "dp pep", "dp positional probability", "dp base scan number", "dp mod scan number"
-				, "dp cluster mass", "dp cluster mass sd", "dp cluster size total", "dp cluster size forward",
-				"dp cluster size reverse", "dp peptide length difference", "dn score", "dn normalized score", "dn nterm mass",
-				"dn cterm mass", "dn score diff", "views", "estimated minutes watched", "average view duration",
-				"average percentage viewed", "subscriber views", "subscriber minutes watched", "clicks", "clickable impressions",
-				"click through rate", "closes", "closable impressions", "close rate", "impressions", "likes", "likes added",
-				"likes removed", "dislikes", "dislikes added", "dislikes removed", "shares", "comments", "favorites",
-				"favorites added", "favorites removed", "subscribers", "subscribers gained", "subscribers lost",
-				"average view duration (minutes)", "scan number", "ion injection time", "total ion current", "base peak intensity",
-				"elapsed time", "precursor full scan number", "precursor intensity", "precursor apex fraction",
-				"precursor apex offset", "precursor apex offset time", "scan event number", "scan index", "ms scan index",
-				"ms scan number", "agc fill", "parent intensity fraction", "intens comp factor", "ctcd comp", "rawovftt",
-				"cycle time", "dead time", "basepeak intensity", "mass calibration", "peak length", "isotope pattern length",
-				"multiplet length", "peaks / s", "single peaks / s", "isotope patterns / s", "single isotope patterns / s",
-				"multiplets / s", "identified multiplets / s", "multiplet identification rate [%]", "ms/ms / s",
-				"identified ms/ms / s", "ms/ms identification rate [%]", "mass fractional part", "mass deficit",
-				"mass precision [ppm]", "max intensity m/z 1", "retention length (fwhm)", "min scan number", "max scan number",
-				"lys count", "arg count", "intensity", "intensity h", "intensity m", "intensity l", "r count", "k count",
-
-			});
-		private static readonly HashSet<string> multiNumericCols =
-			new HashSet<string>(new[]{"mass deviations [da]", "mass deviations [ppm]", "number of phospho (sty)"});
 		public string Filter { get; set; }
 		public PerseusLoadParameterControl() : this(new string[0]) {}
 		public PerseusLoadParameterControl(IList<string> items) : this(items, null) {}
 
 		public PerseusLoadParameterControl(IList<string> items, string filename){
 			InitializeComponent();
-			multiListSelector1.Init(items, new[]{"Expression", "Numerical", "Categorical", "Text", "Multi-numerical"});
+			MultiListSelector1.Init(items, new[]{"Expression", "Numerical", "Categorical", "Text", "Multi-numerical"});
 			if (!string.IsNullOrEmpty(filename)){
 				UpdateFile(filename);
 			}
 		}
 
-		public string Filename { get { return textBox1.Text; } }
-		public int[] ExpressionColumnIndices { get { return multiListSelector1.GetSelectedIndices(0); } }
-		public int[] NumericalColumnIndices { get { return multiListSelector1.GetSelectedIndices(1); } }
-		public int[] CategoryColumnIndices { get { return multiListSelector1.GetSelectedIndices(2); } }
-		public int[] TextColumnIndices { get { return multiListSelector1.GetSelectedIndices(3); } }
-		public int[] MultiNumericalColumnIndices { get { return multiListSelector1.GetSelectedIndices(4); } }
+		public string Filename { get { return TextBox1.Text; } }
+		public int[] ExpressionColumnIndices { get { return MultiListSelector1.GetSelectedIndices(0); } }
+		public int[] NumericalColumnIndices { get { return MultiListSelector1.GetSelectedIndices(1); } }
+		public int[] CategoryColumnIndices { get { return MultiListSelector1.GetSelectedIndices(2); } }
+		public int[] TextColumnIndices { get { return MultiListSelector1.GetSelectedIndices(3); } }
+		public int[] MultiNumericalColumnIndices { get { return MultiListSelector1.GetSelectedIndices(4); } }
 		public string[] Value{
 			get{
 				string[] result = new string[7];
 				result[0] = Filename;
-				result[1] = StringUtils.Concat(";", multiListSelector1.items);
+				result[1] = StringUtils.Concat(";", MultiListSelector1.items);
 				result[2] = StringUtils.Concat(";", ExpressionColumnIndices);
 				result[3] = StringUtils.Concat(";", NumericalColumnIndices);
 				result[4] = StringUtils.Concat(";", CategoryColumnIndices);
@@ -94,11 +43,11 @@ namespace PerseusPluginLib.Load{
 				return result;
 			}
 			set{
-				textBox1.Text = value[0];
-				multiListSelector1.items = value[1].Length > 0 ? value[1].Split(';') : new string[0];
+				TextBox1.Text = value[0];
+				MultiListSelector1.items = value[1].Length > 0 ? value[1].Split(';') : new string[0];
 				for (int i = 0; i < 5; i++){
 					foreach (int ind in GetIndices(value[i + 2])){
-						multiListSelector1.SetSelected(i, ind, true);
+						MultiListSelector1.SetSelected(i, ind, true);
 					}
 				}
 			}
@@ -114,7 +63,7 @@ namespace PerseusPluginLib.Load{
 		}
 
 		internal void UpdateFile(string filename){
-			textBox1.Text = filename;
+			TextBox1.Text = filename;
 			bool csv = filename.ToLower().EndsWith(".csv");
 			char separator = csv ? ',' : '\t';
 			string[] colNames;
@@ -150,88 +99,15 @@ namespace PerseusPluginLib.Load{
 				MessageBox.Show(msg);
 				return;
 			}
-			multiListSelector1.Init(colNames);
+			MultiListSelector1.Init(colNames);
 			if (colTypes != null){
-				SelectExact(colNames, colTypes, colVisible);
+				PerseusUtils.SelectExact(colNames, colTypes, colVisible, MultiListSelector1);
 			} else{
-				SelectHeuristic(colNames);
+				PerseusUtils.SelectHeuristic(colNames, MultiListSelector1);
 			}
 		}
 
-		private void SelectExact(ICollection<string> colNames, IList<string> colTypes, IList<bool> colVisible){
-			for (int i = 0; i < colNames.Count; i++){
-				if (colVisible == null || colVisible[i]){
-					switch (colTypes[i]){
-						case "E":
-							multiListSelector1.SetSelected(0, i, true);
-							break;
-						case "N":
-							multiListSelector1.SetSelected(1, i, true);
-							break;
-						case "C":
-							multiListSelector1.SetSelected(2, i, true);
-							break;
-						case "T":
-							multiListSelector1.SetSelected(3, i, true);
-							break;
-						case "M":
-							multiListSelector1.SetSelected(4, i, true);
-							break;
-						default:
-							throw new Exception("Unknown type: " + colTypes[i]);
-					}
-				}
-			}
-		}
-
-		private void SelectHeuristic(IList<string> colNames){
-			char guessedType = GuessSilacType(colNames);
-			for (int i = 0; i < colNames.Count; i++){
-				if (categoricalCols.Contains(colNames[i].ToLower())){
-					multiListSelector1.SetSelected(2, i, true);
-					continue;
-				}
-				if (textualCols.Contains(colNames[i].ToLower())){
-					multiListSelector1.SetSelected(3, i, true);
-					continue;
-				}
-				if (numericCols.Contains(colNames[i].ToLower())){
-					multiListSelector1.SetSelected(1, i, true);
-					continue;
-				}
-				if (multiNumericCols.Contains(colNames[i].ToLower())){
-					multiListSelector1.SetSelected(4, i, true);
-					continue;
-				}
-				switch (guessedType){
-					case 's':
-						if (colNames[i].StartsWith("Norm. Intensity")){
-							multiListSelector1.SetSelected(0, i, true);
-						}
-						break;
-					case 'd':
-						if (colNames[i].StartsWith("Ratio H/L Normalized ")){
-							multiListSelector1.SetSelected(0, i, true);
-						}
-						break;
-				}
-			}
-		}
-
-		private static char GuessSilacType(IEnumerable<string> colnames){
-			bool isSilac = false;
-			foreach (string s in colnames){
-				if (s.StartsWith("Ratio M/L")){
-					return 't';
-				}
-				if (s.StartsWith("Ratio H/L")){
-					isSilac = true;
-				}
-			}
-			return isSilac ? 'd' : 's';
-		}
-
-		public string Text { get { return textBox1.Text; } set { textBox1.Text = value; } }
+		public string Text { get { return TextBox1.Text; } set { TextBox1.Text = value; } }
 
 		private void Button1_OnClick(object sender, RoutedEventArgs e){
 			Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
