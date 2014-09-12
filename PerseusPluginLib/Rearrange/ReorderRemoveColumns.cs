f292f4b01cfb12e52904e8c4a42453edbbe333fb
unknown <cox@pcmann231.w2k.biochem.mpg.de>
2014-09-12 11:30:06 +0200

    .

--- PerseusPluginLib/Rearrange/ReorderRemoveColumns.cs
+++ PerseusPluginLib/Rearrange/ReorderRemoveColumns.cs
@@ -8,7 +8,7 @@ using PerseusApi.Matrix;
 using PerseusPluginLib.Utils;
 
 namespace PerseusPluginLib.Rearrange{
-	public class ReorderColumns : IMatrixProcessing{
+	public class ReorderRemoveColumns : IMatrixProcessing{
 		public bool HasButton { get { return false; } }
 		public Bitmap DisplayImage { get { return null; } }
 		public string HelpOutput { get { return "Same matrix but with columns in the new order."; } }
@@ -21,8 +21,8 @@ namespace PerseusPluginLib.Rearrange{
 		public string[] HelpDocuments { get { return new string[0]; } }
 		public int NumDocuments { get { return 0; } }
 		public string Url { get { return "http://141.61.102.17/perseus_doku/doku.php?id=perseus:activities:MatrixProcessing:Rearrange:ReorderColumns"; } }
-		public string Description
-		{
+
+		public string Description{
 			get{
 				return
 					"The order of the columns as they appear in the matrix can be changed. Columns can also be omitted. For example, " +
@@ -30,9 +30,7 @@ namespace PerseusPluginLib.Rearrange{
 			}
 		}
 
-		public int GetMaxThreads(Parameters parameters) {
-			return 1;
-		}
+		public int GetMaxThreads(Parameters parameters) { return 1; }
 
 		public void ProcessData(IMatrixData data, Parameters param, ref IMatrixData[] supplTables,
 			ref IDocumentData[] documents, ProcessInfo processInfo){
@@ -56,7 +54,7 @@ namespace PerseusPluginLib.Rearrange{
 			data.StringColumnDescriptions = ArrayUtils.SubList(data.StringColumnDescriptions, textColInds);
 		}
 
-		public Parameters GetParameters(IMatrixData mdata, ref string errorString) {
+		public Parameters GetParameters(IMatrixData mdata, ref string errorString){
 			List<string> exCols = mdata.ExpressionColumnNames;
 			List<string> numCols = mdata.NumericColumnNames;
 			List<string> multiNumCols = mdata.MultiNumericColumnNames;
@@ -65,23 +63,28 @@ namespace PerseusPluginLib.Rearrange{
 			return
 				new Parameters(new Parameter[]{
 					new MultiChoiceParam("Expression columns"){
-						Value = ArrayUtils.ConsecutiveInts(exCols.Count), Values = exCols,
+						Value = ArrayUtils.ConsecutiveInts(exCols.Count),
+						Values = exCols,
 						Help = "Specify here the new order in which the expression columns should appear."
 					},
 					new MultiChoiceParam("Numerical columns"){
-						Value = ArrayUtils.ConsecutiveInts(numCols.Count), Values = numCols,
+						Value = ArrayUtils.ConsecutiveInts(numCols.Count),
+						Values = numCols,
 						Help = "Specify here the new order in which the numerical columns should appear."
 					},
 					new MultiChoiceParam("Multi-numerical columns"){
-						Value = ArrayUtils.ConsecutiveInts(multiNumCols.Count), Values = multiNumCols,
+						Value = ArrayUtils.ConsecutiveInts(multiNumCols.Count),
+						Values = multiNumCols,
 						Help = "Specify here the new order in which the numerical columns should appear."
 					},
 					new MultiChoiceParam("Categorical columns"){
-						Value = ArrayUtils.ConsecutiveInts(catCols.Count), Values = catCols,
+						Value = ArrayUtils.ConsecutiveInts(catCols.Count),
+						Values = catCols,
 						Help = "Specify here the new order in which the categorical columns should appear."
 					},
 					new MultiChoiceParam("Text columns"){
-						Value = ArrayUtils.ConsecutiveInts(textCols.Count), Values = textCols,
+						Value = ArrayUtils.ConsecutiveInts(textCols.Count),
+						Values = textCols,
 						Help = "Specify here the new order in which the text columns should appear."
 					}
 				});
