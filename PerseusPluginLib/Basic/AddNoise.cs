4f162c4a8bfa5ad9bb29613d5ed037fb2e780ab8
Juergen Cox <cox@biochem.mpg.de>
2015-04-24 17:47:28 +0200

    .

--- PerseusPluginLib/Basic/AddNoise.cs
+++ PerseusPluginLib/Basic/AddNoise.cs
@@ -1,43 +1,105 @@
+using System.Collections.Generic;
 using System.Drawing;
 using BaseLib.Param;
 using BaseLibS.Num;
 using BaseLibS.Param;
+using BaseLibS.Util;
 using PerseusApi.Document;
 using PerseusApi.Generic;
 using PerseusApi.Matrix;
 
 namespace PerseusPluginLib.Basic{
 	public class AddNoise : IMatrixProcessing{
-		public bool HasButton { get { return false; } }
-		public Bitmap DisplayImage { get { return null; } }
-		public string Description { get { return "Modulate the data with Gaussian noise."; } }
-		public string HelpOutput { get { return "Same as input matrix with random noise added to the expression columns."; } }
-		public string[] HelpSupplTables { get { return new string[0]; } }
-		public int NumSupplTables { get { return 0; } }
-		public string Name { get { return "Add noise"; } }
-		public string Heading { get { return "Basic"; } }
-		public bool IsActive { get { return true; } }
-		public float DisplayRank { get { return 200; } }
-		public string[] HelpDocuments { get { return new string[0]; } }
-		public int NumDocuments { get { return 0; } }
-		public int GetMaxThreads(Parameters parameters) { return 1; }
-		public string Url { get { return "http://141.61.102.17/perseus_doku/doku.php?id=perseus:activities:MatrixProcessing:Basic:AddNoise"; } }
+		public bool HasButton{
+			get { return false; }
+		}
+
+		public Bitmap DisplayImage{
+			get { return null; }
+		}
+
+		public string Description{
+			get { return "Modulate the data with Gaussian noise."; }
+		}
+
+		public string HelpOutput{
+			get { return "Same as input matrix with random noise added to the expression columns."; }
+		}
+
+		public string[] HelpSupplTables{
+			get { return new string[0]; }
+		}
+
+		public int NumSupplTables{
+			get { return 0; }
+		}
+
+		public string Name{
+			get { return "Add noise"; }
+		}
+
+		public string Heading{
+			get { return "Basic"; }
+		}
+
+		public bool IsActive{
+			get { return true; }
+		}
+
+		public float DisplayRank{
+			get { return 200; }
+		}
+
+		public string[] HelpDocuments{
+			get { return new string[0]; }
+		}
+
+		public int NumDocuments{
+			get { return 0; }
+		}
+
+		public int GetMaxThreads(Parameters parameters){
+			return 1;
+		}
+
+		public string Url{
+			get { return "http://141.61.102.17/perseus_doku/doku.php?id=perseus:activities:MatrixProcessing:Basic:AddNoise"; }
+		}
 
 		public void ProcessData(IMatrixData mdata, Parameters param, ref IMatrixData[] supplTables,
-			ref IDocumentData[] documents, ProcessInfo processInfo){
+		                        ref IDocumentData[] documents, ProcessInfo processInfo){
 			Random2 rand = new Random2();
 			double std = param.GetParam<double>("Standard deviation").Value;
-			for (int i = 0; i < mdata.RowCount; i++){
-				for (int j = 0; j < mdata.ColumnCount; j++){
+			int[] inds = param.GetParam<int[]>("Columns").Value;
+			List<int> mainInds = new List<int>();
+			List<int> numInds = new List<int>();
+			foreach (var ind in inds){
+				if (ind < mdata.ColumnCount){
+					mainInds.Add(ind);
+				} else{
+					numInds.Add(ind - mdata.ColumnCount);
+				}
+			}
+			foreach (int j in mainInds){
+				for (int i = 0; i < mdata.RowCount; i++){
 					mdata.Values[i, j] += (float) rand.NextGaussian(0, std);
+				}
+			}
+			foreach (int j in numInds){
+				for (int i = 0; i < mdata.RowCount; i++){
+					mdata.NumericColumns[j][i] += (float) rand.NextGaussian(0, std);
 				}
 			}
 		}
 
 		public Parameters GetParameters(IMatrixData mdata, ref string errorString){
 			return
-				new Parameters(new Parameter[]
-				{new DoubleParam("Standard deviation", 0.1){Help = "Standard deviation of the noise distribution."}});
+				new Parameters(new Parameter[]{
+					new DoubleParam("Standard deviation", 0.1){Help = "Standard deviation of the noise distribution."},
+					new MultiChoiceParam("Columns", ArrayUtils.ConsecutiveInts(mdata.ColumnCount)){
+						Values = ArrayUtils.Concat(mdata.ColumnNames, mdata.NumericColumnNames)
+					}
+				});
 		}
 	}
 }
--- PerseusPluginLib/Impute/ReplaceMissingByConstant.cs
+++ PerseusPluginLib/Impute/ReplaceMissingByConstant.cs
@@ -1,24 +1,61 @@
+using System.Collections.Generic;
 using System.Drawing;
 using BaseLib.Param;
 using BaseLibS.Param;
+using BaseLibS.Util;
 using PerseusApi.Document;
 using PerseusApi.Generic;
 using PerseusApi.Matrix;
 
 namespace PerseusPluginLib.Impute{
 	public class ReplaceMissingByConstant : IMatrixProcessing{
-		public bool HasButton { get { return false; } }
-		public Bitmap DisplayImage { get { return null; } }
-		public string Description { get { return "Replaces all missing values in expression columns with a constant."; } }
-		public string HelpOutput { get { return "Same matrix but with missing values replaced."; } }
-		public string[] HelpSupplTables { get { return new string[0]; } }
-		public int NumSupplTables { get { return 0; } }
-		public string Name { get { return "Replace missing values by constant"; } }
-		public string Heading { get { return "Imputation"; } }
-		public bool IsActive { get { return true; } }
-		public float DisplayRank { get { return 1; } }
-		public string[] HelpDocuments { get { return new string[0]; } }
-		public int NumDocuments { get { return 0; } }
+		public bool HasButton{
+			get { return false; }
+		}
+
+		public Bitmap DisplayImage{
+			get { return null; }
+		}
+
+		public string Description{
+			get { return "Replaces all missing values in expression columns with a constant."; }
+		}
+
+		public string HelpOutput{
+			get { return "Same matrix but with missing values replaced."; }
+		}
+
+		public string[] HelpSupplTables{
+			get { return new string[0]; }
+		}
+
+		public int NumSupplTables{
+			get { return 0; }
+		}
+
+		public string Name{
+			get { return "Replace missing values by constant"; }
+		}
+
+		public string Heading{
+			get { return "Imputation"; }
+		}
+
+		public bool IsActive{
+			get { return true; }
+		}
+
+		public float DisplayRank{
+			get { return 1; }
+		}
+
+		public string[] HelpDocuments{
+			get { return new string[0]; }
+		}
+
+		public int NumDocuments{
+			get { return 0; }
+		}
 
 		public string Url{
 			get{
@@ -27,26 +64,50 @@ namespace PerseusPluginLib.Impute{
 			}
 		}
 
-		public int GetMaxThreads(Parameters parameters) { return 1; }
+		public int GetMaxThreads(Parameters parameters){
+			return 1;
+		}
 
 		public void ProcessData(IMatrixData mdata, Parameters param, ref IMatrixData[] supplTables,
-			ref IDocumentData[] documents, ProcessInfo processInfo){
+		                        ref IDocumentData[] documents, ProcessInfo processInfo){
 			float value = (float) param.GetParam<double>("Value").Value;
-			ReplaceMissingsByVal(value, mdata);
+			int[] inds = param.GetParam<int[]>("Columns").Value;
+			List<int> mainInds = new List<int>();
+			List<int> numInds = new List<int>();
+			foreach (int ind in inds){
+				if (ind < mdata.ColumnCount){
+					mainInds.Add(ind);
+				} else{
+					numInds.Add(ind - mdata.ColumnCount);
+				}
+			}
+			ReplaceMissingsByVal(value, mdata, mainInds, numInds);
 		}
 
 		public Parameters GetParameters(IMatrixData mdata, ref string errorString){
 			return
-				new Parameters(new Parameter[]
-				{new DoubleParam("Value", 0){Help = "The value that is going to be filled in for missing values."}});
+				new Parameters(new Parameter[]{
+					new DoubleParam("Value", 0){Help = "The value that is going to be filled in for missing values."},
+					new MultiChoiceParam("Columns", ArrayUtils.ConsecutiveInts(mdata.ColumnCount)){
+						Values = ArrayUtils.Concat(mdata.ColumnNames, mdata.NumericColumnNames)
+					}
+				});
 		}
 
-		public static void ReplaceMissingsByVal(float value, IMatrixData data){
-			for (int i = 0; i < data.RowCount; i++){
-				for (int j = 0; j < data.ColumnCount; j++){
+		private static void ReplaceMissingsByVal(float value, IMatrixData data, IEnumerable<int> mainInds,
+		                                         IEnumerable<int> numInds){
+			foreach (int j in mainInds){
+				for (int i = 0; i < data.RowCount; i++){
 					if (float.IsNaN(data.Values[i, j])){
 						data.Values[i, j] = value;
 						data.IsImputed[i, j] = true;
+					}
+				}
+			}
+			foreach (int j in numInds){
+				for (int i = 0; i < data.RowCount; i++){
+					if (double.IsNaN(data.NumericColumns[j][i])){
+						data.NumericColumns[j][i] = value;
 					}
 				}
 			}
