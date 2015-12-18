0233570be69e8b0c4b080e39f8b532e206d99827
JurgenCox <cox@biochem.mpg.de>
2015-12-18 06:15:01 +0100

    .

--- PerseusPluginLib/Group/JoinTermsInCategoricalRow.cs
+++ PerseusPluginLib/Group/JoinTermsInCategoricalRow.cs
@@ -7,58 +7,65 @@ using PerseusApi.Document;
 using PerseusApi.Generic;
 using PerseusApi.Matrix;
 
-namespace PerseusPluginLib.Group {
-	public class JoinTermsInCategoricalRow : IMatrixProcessing {
-		public bool HasButton { get { return false; } }
-		public Bitmap DisplayImage { get { return null; } }
-		public string Description { get { return "The selected terms in the categorical row will be joined to one term."; } }
-		public string HelpOutput { get { return "The filtered matrix."; } }
-		public string[] HelpSupplTables { get { return new string[0]; } }
-		public int NumSupplTables { get { return 0; } }
-		public string Name { get { return "Join terms in categorical row"; } }
-		public string Heading { get { return "Annot. rows"; } }
-		public bool IsActive { get { return true; } }
-		public float DisplayRank { get { return 20; } }
-		public string[] HelpDocuments { get { return new string[0]; } }
-		public int NumDocuments { get { return 0; } }
-		public string Url { get { return "http://coxdocs.org/doku.php?id=perseus:user:activities:MatrixProcessing:Annotrows:JoinTermsInCategoricalRow"; } }
+namespace PerseusPluginLib.Group{
+	public class JoinTermsInCategoricalRow : IMatrixProcessing{
+		public bool HasButton => false;
+		public Bitmap DisplayImage => null;
+		public string Description => "The selected terms in the categorical row will be joined to one term.";
+		public string HelpOutput => "The filtered matrix.";
+		public string[] HelpSupplTables => new string[0];
+		public int NumSupplTables => 0;
+		public string Name => "Join terms in categorical row";
+		public string Heading => "Annot. rows";
+		public bool IsActive => true;
+		public float DisplayRank => 20;
+		public string[] HelpDocuments => new string[0];
+		public int NumDocuments => 0;
 
-		public int GetMaxThreads(Parameters parameters) {
+		public string Url
+			=> "http://coxdocs.org/doku.php?id=perseus:user:activities:MatrixProcessing:Annotrows:JoinTermsInCategoricalRow";
+
+		public int GetMaxThreads(Parameters parameters){
 			return 1;
 		}
 
-		public Parameters GetParameters(IMatrixData mdata, ref string errorString) {
+		public Parameters GetParameters(IMatrixData mdata, ref string errorString){
 			Parameters[] subParams = new Parameters[mdata.CategoryRowCount];
-			for (int i = 0; i < mdata.CategoryRowCount; i++) {
+			for (int i = 0; i < mdata.CategoryRowCount; i++){
 				string[] values = mdata.GetCategoryRowValuesAt(i);
-				int[] sel = values.Length == 1 ? new[] { 0 } : new int[0];
+				int[] sel = values.Length == 1 ? new[]{0} : new int[0];
 				subParams[i] =
 					new Parameters(new Parameter[]{
-						new MultiChoiceParam("Values", sel)
-						{Values = values, Help = "The value that should be present to discard/keep the corresponding row."}
+						new MultiChoiceParam("Values", sel){
+							Values = values,
+							Help = "The value that should be present to discard/keep the corresponding row."
+						}
 					});
 			}
 			return
 				new Parameters(new Parameter[]{
 					new SingleChoiceWithSubParams("Row"){
-						Values = mdata.CategoryRowNames, SubParams = subParams,
-						Help = "The categorical row that the filtering should be based on.", ParamNameWidth = 50, TotalWidth = 731
+						Values = mdata.CategoryRowNames,
+						SubParams = subParams,
+						Help = "The categorical row that the filtering should be based on.",
+						ParamNameWidth = 50,
+						TotalWidth = 731
 					},
-					new StringParam("New term") 
+					new StringParam("New term")
 				});
 		}
 
 		public void ProcessData(IMatrixData mdata, Parameters param, ref IMatrixData[] supplTables,
-			ref IDocumentData[] documents, ProcessInfo processInfo) {
-				ParameterWithSubParams<int> p = param.GetParamWithSubParams<int>("Row");
+			ref IDocumentData[] documents, ProcessInfo processInfo){
+			ParameterWithSubParams<int> p = param.GetParamWithSubParams<int>("Row");
 			int colInd = p.Value;
-			if (colInd < 0) {
+			if (colInd < 0){
 				processInfo.ErrString = "No categorical rows available.";
 				return;
 			}
 			Parameter<int[]> mcp = p.GetSubParameters().GetParam<int[]>("Values");
 			int[] inds = mcp.Value;
-			if (inds.Length < 1) {
+			if (inds.Length < 1){
 				processInfo.ErrString = "Please select at least two terms for merging.";
 				return;
 			}
@@ -67,9 +74,8 @@ namespace PerseusPluginLib.Group {
 				processInfo.ErrString = "Please specify a new term.";
 				return;
 			}
-
 			string[] values = new string[inds.Length];
-			for (int i = 0; i < values.Length; i++) {
+			for (int i = 0; i < values.Length; i++){
 				values[i] = mdata.GetCategoryRowValuesAt(colInd)[inds[i]];
 			}
 			HashSet<string> value = new HashSet<string>(values);
