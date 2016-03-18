d273e0543532ff30d0983841ce35effcc3352794
JurgenCox <cox@biochem.mpg.de>
2016-03-18 21:38:59 +0100

    .

--- PerseusPluginLib/Impute/ReplaceImputedByNan.cs
+++ PerseusPluginLib/Impute/ReplaceImputedByNan.cs
@@ -15,7 +15,7 @@ namespace PerseusPluginLib.Impute{
 		public string Name => "Replace imputed values by NaN";
 		public string Heading => "Imputation";
 		public bool IsActive => true;
-		public float DisplayRank => 5;
+		public float DisplayRank => 10;
 		public string[] HelpDocuments => new string[0];
 		public int NumDocuments => 0;
 
