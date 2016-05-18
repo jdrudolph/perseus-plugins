4f6481f24efc4efbea844b118295ca30c3362c02
Jan Rudolph <rudolph@biochem.mpg.de>
2016-05-18 12:22:51 +0200

    snapshot

---
+++ PerseusApi/PerseusApi.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Claims;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PerseusApi.Utils;
+
+namespace PerseusApi.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] baseNames;
+            string[] files;
+            var x = 12;
+
+            PerseusUtils.GetAvailableAnnots(out baseNames, out files);
+
+            Assert.Fail();
+        }
+    }
+}
